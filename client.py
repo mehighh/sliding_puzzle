8b65abe660a014b46a3bc13c9b3d8a5c4c8d16d8
Vesztergombi Mihály <mehigh@debian>
2019-07-23 15:25:10 +0200

    working on client GUI

--- client.py
+++ client.py
@@ -7,159 +7,194 @@ import pygame
 from game import Game
 
 
+class Client:
 
-HEADER_LENGTH  = 15
-PORT           = 5555
+  # header format:
+  # message_size(int) + " " + option
+  #
+  # options:
+  #
+  #  CLIENT
+  #  - "init": initial message from client username
+  #  - "ready": if client in ready
+  #  - "result": client game results
+  #
+  #  SERVER
+  #  - "board": server sending the shuffled board to the players
+  #  - "readyinf": other players being ready
+  #  - "stat": game statistics
 
-try:
-  # get IP
-  s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-  s.connect(("8.8.8.8", 80))
-  IP = s.getsockname()[0]
-  s.close()
-except:
-  print("No network -> local only")
-  IP = "127.0.0.1"
+  def __init__(self):
 
-print(f"IP: {IP}")
-print("If this does not match the server IP try typing that in (otherwise just hit Enter)")
-dif_ip = input("Type different IP: ")
-if len(dif_ip):
-  IP = dif_ip
+    self.HEADER_LENGTH  = 15
+    self.PORT           = 5555
 
-client_username = input("\nUsername: ")
-# if the username is 'reader', the client can not send messages, but sees all new messages inmediately
+    try:
+      # get IP
+      s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+      s.connect(("8.8.8.8", 80))
+      self.IP = s.getsockname()[0]
+      s.close()
+    except:
+      print("No network -> local only")
+      self.IP = "127.0.0.1"
 
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect((IP, PORT))
-client_socket.setblocking(False)
+    print(f"IP: {self.IP}")
+    print("If this does not match the server IP try typing that in (otherwise just hit Enter)")
+    dif_ip = input("Type different IP: ")
+    if len(dif_ip):
+      self.IP = dif_ip
 
+    self.username = input("\nUsername: ")
 
-
-
-# indicates if client is ready
-client_ready = False
-RUN = True
-
-# header format:
-# message_size(int) + " " + option
-#
-# options:
-#
-#  CLIENT
-#  - "init": initial message from client username
-#  - "user": send username -> followed by second message
-#  - "ready": if client in ready
-#  - "result": client game results
-#
-#  SERVER
-#  - "board": server sending the shuffled board to the players
-#  - "readyinf": other players being ready
-#  - "stat": game statistics
-
-def send_message(option, message):
-  if not len(message):
-    print("Message is empty -> not sending")
-    return False
-
-  if option not in ["init", "ready", "result"]:
-    print("Wrong header option")
-    return False
+    self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    self.socket.connect((self.IP, self.PORT))
+    self.socket.setblocking(False)
 
 
-  message = message.encode("utf-8")
-  msgstr = f"{len(message)} {option}"
-  message_header = f"{msgstr:<{HEADER_LENGTH}}".encode('utf-8')
-  client_socket.send(message_header + message)
+    # GUI stuff
+    pygame.init()
+    pygame.font.init()
+
+    self.SIZE       = (400,400)
+    self.GRID_SIZE  = int(self.SIZE[0]/4)
+    self.SCREEN     = pygame.display.set_mode(self.SIZE)
+    self.FONT_SIZE  = 20
+    self.FONT       = pygame.font.SysFont('Monospace', self.FONT_SIZE)
 
 
-def receive_message():
-  try:
-    # get message header
-    message_header = client_socket.recv(HEADER_LENGTH)
+    # send first message -> server saves username, header
+    self.send_message("init", self.username)
 
-    # if there is no message
-    if not len(message_header):
+    # indicates if client is ready
+    self.READY  = False
+    self.RUN    = True
+    self.INFO   = []
+
+  def send_message(self, option, message):
+    if not len(message):
+      print("Message is empty -> not sending")
       return False
 
-    # get message length and option
-    message_length = int(message_header.decode("utf-8").split()[0])
-    option =         str(message_header.decode("utf-8").split()[1])
-
-    # get message
-    message = client_socket.recv(message_length)
-
-    if option not in ["board", "readyinf", "stat"]:
+    if option not in ["init", "ready", "result"]:
       print("Wrong header option")
       return False
 
-    elif option == "readyinf":
-      info = pickle.loads(message)
-      print('\nNot ready:')
 
-      for cli in info[0]:
-        print(f"  {cli}")
-
-      print('Ready:')
-
-      for cli in info[1]:
-        print(f"  {cli}")
-
-    elif option == "board":
-      print('Recieveing boeard')
-      board = pickle.loads(message)
-
-      pygame.init()
-      SIZE = (400,400)
-      S = pygame.display.set_mode(SIZE)
-      GAME = Game(S)
-      GAME.board = board[:]
-      GAME.run()
-
-      stats = f"{GAME.steps} {GAME.time}"
-      send_message("result", stats)
-      print("\nwaiting for others...")
-
-    elif option == "stat":
-      stat_board = pickle.loads(message)
-      for row in stat_board:
-        print(row)
-      quit()
-
-  except Exception as e:
-    # print(f"Recieve error {str(e)}")
-    return False
+    message = message.encode("utf-8")
+    msgstr = f"{len(message)} {option}"
+    message_header = f"{msgstr:<{self.HEADER_LENGTH}}".encode('utf-8')
+    self.socket.send(message_header + message)
 
 
-def quit():
-  print("Exiting")
-  exit()
-
-
-if __name__ == '__main__':
-  # send first message -> server saves username, header
-  send_message("init", client_username)
-
-  while RUN:
-    message = ""
-
+  def receive_message(self):
     try:
-      # recieve stuff
-      receive_message()
+      # get message header
+      message_header = self.socket.recv(self.HEADER_LENGTH)
 
-    except IOError as e:
-      if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
-        print('Reading error', str(e))
-        exit()
-      continue
+      # if there is no message
+      if not len(message_header):
+        return False
+
+      # get message length and option
+      message_length = int(message_header.decode("utf-8").split()[0])
+      option =         str(message_header.decode("utf-8").split()[1])
+
+      # get message
+      message = self.socket.recv(message_length)
+
+      if option not in ["board", "readyinf", "stat"]:
+        print("Wrong header option")
+        return False
+
+      elif option == "readyinf":
+        self.INFO = pickle.loads(message)[:]
+        print('\nNot ready:')
+
+        for cli in self.INFO[0]:
+          print(f"  {cli}")
+
+        print('Ready:')
+
+        for cli in self.INFO[1]:
+          print(f"  {cli}")
+
+      elif option == "board":
+        print('Recieveing boeard')
+        board = pickle.loads(message)
+
+        GAME = Game(SCREEN)
+        GAME.board = board[:]
+        GAME.run()
+
+        stats = f"{GAME.steps} {GAME.time}"
+        self.send_message("result", stats)
+        print("\nwaiting for others...")
+
+      elif option == "stat":
+        stat_board = pickle.loads(message)
+        for row in stat_board:
+          print(row)
+        quit()
 
     except Exception as e:
-      print(str(e))
-      exit()
+      # print(f"Recieve error {str(e)}")
+      return False
 
-    if client_username != 'reader':
-      if not client_ready:
-        message = input("Are you ready? (y/n) : ").lower()
-        if message == 'y':
-          client_ready = True
-          send_message("ready", message)
+
+  def quit(self):
+    print("Exiting")
+    exit()
+
+
+  def draw(self):
+    readytext  = self.FONT.render(f'Ready:',     False, (255,255,255))
+    nreadytext = self.FONT.render(f'Not ready:', False, (255,255,255))
+
+    self.SCREEN.blit(readytext,  (0              ,0))
+    self.SCREEN.blit(nreadytext, (int(self.SIZE[0]/2) ,0))
+
+    if len(self.INFO):
+      for i,nr in enumerate(self.INFO[0]):
+        textsurface = self.FONT.render(f'{nr}', False, (255,255,255))
+        self.SCREEN.blit(textsurface, (int(self.SIZE[0]/2), (i+1)*self.FONT_SIZE))
+
+      for i,r in enumerate(self.INFO[1]):
+        textsurface = self.FONT.render(f'{r}', False, (255,255,255))
+        self.SCREEN.blit(textsurface, (0, (i+1)*self.FONT_SIZE))
+
+    if not self.READY:
+      pygame.draw.rect(self.SCREEN, (255,0,0), (self.SIZE[0]-20, self.SIZE[0]-20, 20, 20), 0)
+
+
+  def run(self):
+    while self.RUN:
+
+      for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+          pygame.quit()
+          exit()
+
+      message = ""
+
+      try:
+        # recieve stuff
+        self.receive_message()
+
+      except IOError as e:
+        if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
+          print('Reading error', str(e))
+          exit()
+        continue
+
+      except Exception as e:
+        print(str(e))
+        exit()
+
+      self.SCREEN.fill(0)
+      self.draw()
+      pygame.display.flip()
+
+if __name__ == '__main__':
+  CLI = Client()
+  CLI.run()
